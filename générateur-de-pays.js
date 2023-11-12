ba3bf74a6302679d9b1811079065262890d91aa9
come.epitalondeguidis <come.epitalondeguidis@monlycee.net>
2023-11-12 14:58:09 +0100

    Fix broken random function

---
+++ générateur-de-pays.js
@@ -0,0 +1,15 @@
+// Tableau contenant les noms de toutes les pages
+const pageList = ["Autriche.html", "Bouthan.html", "Islande.html", "Kirghizistan.html", "Norvège.html", "Madagascar.html", "Nouvelle-Zélande.html", "Pérou.html", "Sénégal.html", "Tanzanie.html"];
+
+
+// Fonction pour rediriger l'utilisateur vers une page aléatoire
+function redirectToRandomPage() {
+  // Générer un nombre aléatoire entre 0 et 9 (index des pages dans le tableau)
+  const randomIndex = Math.floor(Math.random() * pageList.length);
+
+  // Récupérer le nom de la page à partir du tableau
+  const randomPage = pageList[randomIndex];
+
+  // Rediriger l'utilisateur vers la page sélectionnée
+  window.location.href = randomPage;
+}
